f899c32b0551d80dbdf040378980a4a17e828b0d
Abhinav8302 <89733898+Abhinav8302@users.noreply.github.com>
2022-01-05 20:35:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function open_bedroom_img(){
+    window.location="button1.html";
+}
+
+function open_image_img(){
+    window.location="button2.html";
+}
+
+function open_fruit_img(){
+    window.location="button3.html";
+}
+
+function open_tv_img(){
+    window.location="button4.html";
+}
+
+function open_laptop_img(){
+    window.location="button5.html";
+}
+
+function open_main_page(){
+    window.location="index.html";
+}
